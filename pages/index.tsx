72a180edadf8ec83543f0e3ef7b6db83afbd99b3
TheBakhodirov <91797611+TheBakhodirov@users.noreply.github.com>
2023-02-28 22:49:38 +0500

    first setup

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Al Quran</title>
+        <meta name="description" content="Holy Quran website with prayer times" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        
+      </main>
+    </>
+  );
+}
