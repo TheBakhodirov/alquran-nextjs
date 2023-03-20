546757b9c022c9cdf9d5573e8aedb1b6ad733a83
TheBakhodirov <91797611+TheBakhodirov@users.noreply.github.com>
2023-03-20 18:14:12 +0500

    changed favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.scss";
-import Navbar from "@/components/Navbar";
 import Link from "next/link";
 import Image from "next/image";
 import Quran from "public/koran.png";
